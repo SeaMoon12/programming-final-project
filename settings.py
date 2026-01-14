0aee482de1930d00fcc4848520acc2323b79b723
S3AM00N <167020878+S3AM00N@users.noreply.github.com>
2026-01-14 23:07:16 +0700

    last commit, there's also anagram minigame done and hangman in test.py

--- settings.py
+++ settings.py
@@ -1,3 +1,14 @@
+# Print speed
 print_speed = 0
+
+# Limit
 searches = 3
 investigator_searches = 1
+
+# Answers to the minigames
+hangman_answer = None
+wordle_answer = None
+numbrle_answer = None
+anagram_answer = None
+cryptic_answer = None
+riddle_answer = None
